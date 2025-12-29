0efa77b9ff93f7b988fe4067d2be7ba2dbee0b60
igora <igoralbuquerque102@hotmail.com>
2025-12-29 13:01:39 -0300

    feat(Automatic mode): :sparkles: Implementado modo automatic
    Foi implementado o modo automatic que entra na spell, usa e sai. Além disso foi corrigido o clearSpell para usar uma referencia a spell salva em MagicState e usar o getFromCaster apenas como fallback.

--- src/MagicState.h
+++ src/MagicState.h
@@ -30,42 +30,54 @@ namespace IntegratedMagic {
             RE::ExtraDataList* extra{nullptr};
         };
 
-        std::vector<ExtraEquippedItem> _prevExtraEquipped;
-
         void TogglePress(int slot);
+        void ToggleAutomatic(int slot);
         bool IsActive() const;
         int ActiveSlot() const;
         void HoldDown(int slot);
         void HoldUp(int slot);
         bool IsHoldActive() const { return _holdActive; }
         int HoldSlot() const { return _holdSlot; }
+        bool IsAutomaticActive() const { return _autoActive; }
+        int AutomaticSlot() const { return _autoSlot; }
         void StartAutoAttack(EquipHand hand);
         void StopAutoAttack();
         void PumpAutoAttack(float dt);
         void NotifyAttackEnabled();
         void RequestAutoAttackStart(EquipHand hand);
         void TryStartWaitingAutoAttack();
+        void PumpAutomatic();
+        void AutoExit();
 
     private:
         void EnterPress(int slot);
         bool ApplyPress(int slot);
         void EnterHold(int slot);
+        void EnterAutomatic(int slot);
         void ExitAll();
 
         void CaptureSnapshot(RE::PlayerCharacter* player);
         void RestoreSnapshot(RE::PlayerCharacter* player);
         void UpdatePrevExtraEquippedForOverlay(const std::function<void()>& equipFn);
 
+        std::vector<ExtraEquippedItem> _prevExtraEquipped;
         bool _active{false};
         int _activeSlot{-1};
         bool _holdActive{false};
         int _holdSlot{-1};
+        bool _autoActive{false};
+        int _autoSlot{-1};
+        EquipHand _autoHand{EquipHand::Right};
+        bool _autoWaitingChargeComplete{false};
+        bool _autoChargeComplete{false};
         HandSnapshot _snap{};
         bool _autoAttackHeld{false};
         EquipHand _autoAttackHand{EquipHand::Right};
         float _autoAttackSecs{0.0f};
-        bool _attackEnabled = false;
-        bool _waitingAutoAfterEquip = false;
-        EquipHand _waitingAutoHand = EquipHand::Right;
+        bool _attackEnabled{false};
+        bool _waitingAutoAfterEquip{false};
+        EquipHand _waitingAutoHand{EquipHand::Right};
+        RE::SpellItem* _modeSpellLeft{nullptr};
+        RE::SpellItem* _modeSpellRight{nullptr};
     };
 }
